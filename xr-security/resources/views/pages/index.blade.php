77859e55e8068b12c793c36bf653fc16d6ca3fa5
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-06 00:50:47 +0200

    Some updates in my application

--- xr-security/resources/views/pages/index.blade.php
+++ xr-security/resources/views/pages/index.blade.php
@@ -247,8 +247,8 @@
                 </div>
                 <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
                     <div class="col">
-                        <div class="card mb-4 rounded-3 shadow-sm">
-                            <div class="card-header py-3">
+                        <div class="card mb-4 shadow-sm" style="border-radius:0;height:40rem;">
+                            <div class="card-header py-3" style="border-radius:0;">
                                 <h4 class="my-0 fw-normal">Free</h4>
                             </div>
                             <div class="card-body">
@@ -266,8 +266,8 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card mb-4 rounded-3 shadow-sm">
-                            <div class="card-header py-3">
+                        <div class="card mb-4 shadow-sm" style="border-radius:0;height:40rem;">
+                            <div class="card-header py-3" style="border-radius:0;">
                                 <h4 class="my-0 fw-normal">Pro</h4>
                             </div>
                             <div class="card-body">
@@ -284,8 +284,8 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card mb-4 rounded-3 shadow-sm border-primary">
-                            <div class="card-header py-3 text-bg-primary border-primary">
+                        <div class="card mb-4 shadow-sm border-primary" style="border-radius:0;height:40rem;">
+                            <div class="card-header py-3 text-bg-primary border-primary" style="border-radius:0;">
                                 <h4 class="my-0 fw-normal">Enterprise</h4>
                             </div>
                             <div class="card-body">
