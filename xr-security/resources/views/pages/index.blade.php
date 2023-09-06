086220735c4824d5b6c677a23a5db6f47fad63c2
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-06 02:28:17 +0200

    Some updates in my application

--- xr-security/resources/views/pages/index.blade.php
+++ xr-security/resources/views/pages/index.blade.php
@@ -508,10 +508,10 @@
                     </div>
 
                     <div class="col-lg-4 col-md-6 footer-newsletter">
-                        <h4>Our Newsletter</h4>
-                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                        <h4>Nosso boletim informativo</h4>
+                        <p>Preencha com o seu email, para ficares a par de todas as novidades.</p>
                         <form action="" method="post">
-                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                            <input type="email" name="email"><input type="submit" value="Increva-se">
                         </form>
 
                     </div>
@@ -522,14 +522,10 @@
 
         <div class="container">
             <div class="copyright">
-                &copy; Copyright <strong><span>Gp</span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span>Gp</span></strong>. Todos direitos reservados
             </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                Desenvolvido pela <a href="">Emilio prestação de multisserviços</a>
             </div>
         </div>
     </footer><!-- End Footer -->
