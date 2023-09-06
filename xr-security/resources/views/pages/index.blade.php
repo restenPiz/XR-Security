74c21afc3219247c36b9877d19566b0b570c61bd
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-06 02:22:17 +0200

    Some updates in my aplication

--- xr-security/resources/views/pages/index.blade.php
+++ xr-security/resources/views/pages/index.blade.php
@@ -470,19 +470,17 @@
 
                     <div class="col-lg-3 col-md-6">
                         <div class="footer-info">
-                            <h3>Gp<span>.</span></h3>
+                            <h3>XR<span>.</span></h3>
                             <p>
-                                A108 Adam Street <br>
-                                NY 535022, USA<br><br>
-                                <strong>Phone:</strong> +1 5589 55488 55<br>
-                                <strong>Email:</strong> info@example.com<br>
+                                Matacuane <br>
+                                Beira, Moçambique<br><br>
+                                <strong>Numero de Telefone:</strong> +258 850660666<br>
+                                <strong>Email:</strong> Xrmozambique@gmail.com<br>
                             </p>
                             <div class="social-links mt-3">
-                                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                                <a href="#" class="twitter"><i class="bx bxl-whatsapp"></i></a>
                                 <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
                                 <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
                             </div>
                         </div>
                     </div>
